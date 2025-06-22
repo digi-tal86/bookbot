30e0006bbafbaadf99e394a675c2dd54bf4425a8
digital86 <pgregg@aagcnc.com>
2025-06-22 16:27:25 +0100

    Bookbot CH: 3 L: 1-3

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import get_word_count
-from stats import get_sorted_character_list
-
-filename = "books/frankenstein.txt"
+import sys
+from stats import get_word_count, get_sorted_character_list
 
 def get_book_text(filename):
-    with open(filename) as f:
-        file_contents = f.read()
-        return file_contents
+    try:
+        with open(filename) as f:
+            file_contents = f.read()
+            return file_contents
+    except:
+        print(f"File does not exist! ({filename})")
+        sys.exit(1)
 
 def main():
+    try:
+        filename = sys.argv[1]
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     text = get_book_text(filename)
     word_count = get_word_count(text)
     character_count = get_sorted_character_list(text)
